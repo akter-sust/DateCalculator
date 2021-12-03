9d30fedc470b35f7ef66cd9f71140543e0a9d323
Md akter Hussain <akter.sust@gmail.com>
2021-12-04 07:36:17 +1100

    handled date validation in the constructor for unittest

--- date.py
+++ date.py
@@ -13,6 +13,8 @@ class Date:
         self.days_in_month = {1: 31,  2: 28,  3: 31,  4: 30,
                               5: 31,  6: 30,  7: 31,  8: 31,
                               9: 30, 10: 31, 11: 30, 12: 31}
+        if not self.is_valid_date():
+            raise ValueError("Wrong Date provided")
         
     def is_leap_year(self):
         """
